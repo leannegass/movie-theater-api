29e00a14762131e34a6626a9188f44530b2599f5
MedoEtman <medo-ammar@hotmail.co.uk>
2023-05-11 22:59:55 +0100

    WIP

--- models/Show.js
+++ models/Show.js
@@ -1,6 +1,8 @@
 //import our db, Model, DataTypes
 const { db, DataTypes } = require('../db/connection')
+const router = express.Router();
 
+ 
 //Creating a User child class from the Model parent class
 const Show = db.define("shows", {
     title: DataTypes.STRING,
@@ -9,5 +11,101 @@ const Show = db.define("shows", {
     status: DataTypes.STRING,
 });
 
+ // code to get all shows
+ router.get('/shows', async (req, res, next) => {
+    try {
+      const shows = await Show.findAll();
+      res.json(shows);
+    } catch (error) {
+      next(error);
+    }
+  });
+  
+// Get one show
+router.get('/shows/:id', async (req, res, next) => {
+    try {
+      const { id } = req.params;
+  
+      const show = await Show.findByPk(id);
+      if (!show) {
+        return res.status(404).json({ error: 'Show not found' });
+      }
+  
+      res.json(show);
+    } catch (error) {
+      next(error);
+    }
+  });
+  
+
+router.get('/shows/genre/:genre', async(req, res, next) => { 
+    try {
+        const { genre } = req.params;
+        const showsAtGenre = await Show.findAll({where: {genre: genre}});
+        if (showsAtGenre.length > 0) {
+            res.json(showsAtGenre);
+        } else {
+            res.sendStatus(404);
+        }
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.put('/shows/:id/rating', async(req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { rating } = req.body;
+
+        const show = await Show.findByPk(id);
+        if (!show) {
+            return res.status(404).json({ error: 'Show not found' });
+        }
+
+        show.rating = rating;
+        await show.save();
+
+        res.json(show);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.put('/shows/:id/status', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { status } = req.body;
+
+    const show = await Show.findByPk(id);
+    if (!show) {
+      return res.status(404).json({ error: 'Show not found' });
+    }
+
+    show.status = status;
+    await show.save();
+
+    res.json(show);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete('/shows/:id', async (req, res, next) => {
+    try {
+      const { id } = req.params;
+  
+      const show = await Show.findByPk(id);
+      if (!show) {
+        return res.status(404).json({ error: 'Show not found' });
+      }
+  
+      await show.destroy();
+  
+      res.json({ message: 'Show deleted successfully' });
+    } catch (error) {
+      next(error);
+    }
+  });
+  
 //exports
 module.exports = Show;
